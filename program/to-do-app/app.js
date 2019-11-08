81bf5ed107b5cb310f599c059523ba71fadf7471
Joseph White <jhwwhite@gmail.com>
2019-11-08 14:14:02 -0600

    11-8-19

--- program/to-do-app/app.js
+++ program/to-do-app/app.js
@@ -1,31 +1,62 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  const toDos = [];
+  let id = 0;
 
-  addToDoForm.addEventListener('submit', event => {
+function createNewToDo() {
+      const newToDoText = document.getElementById('newToDoText');
+      toDos.push({
+        title: newToDoText.value,
+        complete: false,
+        id: id
+      });
+
+    id++;
+
+      newToDoText.value = '';
+
+      renderTheUI();
+  }
+
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete!';
+
+      deleteButton.addEventListener('click', event => {
+       alert(toDo.title)
+       toDos =toDos.filter(functio(item){
+         return item.id !== toDo.id
+       })
+       renderTheUI();
+     });
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+    });
+  }
+
+addToDoForm.addEventListener('submit',  => {
+
     event.preventDefault();
-
-    let title = newToDoText.value;
-
-    let newLi = document.createElement('Li');
-
-    let checkbox = document.createElement('input');
-
-    checkbox.type = "checkbox";
-
-    newLi.textContent = title;
-
-    newLi.appendChild(checkbox);
-
-    toDoList.appendChild(newLi);
-
-    newToDoText.value = '';
-
+    createNewToDo();
 
   });
-}
-window.onload = function(){
-  alert("The window has loaded");
+
+renderTheUI();
+  };
+
+window.onload = function() {
   onReady();
 };
